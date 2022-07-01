572bc7b4107a0ac44d5f52bd5a82a23be99ddc63
Pablo Riesgo Ferreiro <priesgoferreiro@gmail.com>
2022-07-01 09:34:21 +0200

    add bgzip compression and tabix index to output VCFs

--- tests/test_02.sh
+++ tests/test_02.sh
@@ -6,4 +6,5 @@ output=output/test2
 echo -e "sample1\t"`pwd`"/test_data/TESTX_S1_L001.bam,"`pwd`"/test_data/TESTX_S1_L001.bam\t"`pwd`"/test_data/TESTX_S1_L003.bam,"`pwd`"/test_data/TESTX_S1_L003.bam" > test_data/test_input.txt
 nextflow main.nf -profile test,conda --output $output --input_files test_data/test_input.txt
 
-test -s $output/sample1/sample1.passed.somatic.vcf || { echo "Missing output VCF!"; exit 1; }
+test -s $output/sample1/sample1.strelka2.somatic.vcf.gz || { echo "Missing output VCF!"; exit 1; }
+test -s $output/sample1/sample1.strelka2.somatic.vcf.gz.tbi || { echo "Missing output index!"; exit 1; }
